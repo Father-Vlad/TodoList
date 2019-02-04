095bd2964123c28c65e47733a4d3735e31aeb74a
Father_Vlad <vladislavshkodich@gmail.com>
2019-02-04 11:08:36 +0200

    Hide the keyboard completed.

--- TodoList.iOS/Views/FillingDataView.cs
+++ TodoList.iOS/Views/FillingDataView.cs
@@ -1,3 +1,4 @@
+using System;
 using MvvmCross.Binding.BindingContext;
 using MvvmCross.Platforms.Ios.Presenters.Attributes;
 using MvvmCross.Platforms.Ios.Views;
@@ -13,12 +14,22 @@ namespace TodoList.iOS.Views
         public FillingDataView() : base(nameof(FillingDataView), null)
         {
         }
+
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
             _buttonGoBack = new UIBarButtonItem(UIBarButtonSystemItem.Reply, null);
             NavigationItem.SetLeftBarButtonItem(_buttonGoBack, false);
-            
+
+            //Hide the keyboard after using it for entering data in a text field
+            NameOfTaskTextField.ShouldReturn = (textField) => TextFieldResignFirstResponder(textField);
+            DescriptionOfTaskTextField.ShouldReturn = (textField) => TextFieldResignFirstResponder(textField);
+
+            //Remove on tapping the view
+            var endEditing = new UITapGestureRecognizer(() => View.EndEditing(true));
+            endEditing.CancelsTouchesInView = false;
+            View.AddGestureRecognizer(endEditing);
+
             var set = this.CreateBindingSet<FillingDataView, FillingDataViewModel>();
             set.Bind(NameOfTaskTextField).To(vm => vm.GoalName);
             set.Bind(DescriptionOfTaskTextField).To(vm => vm.GoalDescription);
@@ -28,6 +39,12 @@ namespace TodoList.iOS.Views
             set.Bind(DeleteButton).To(vm => vm.DeleteDataCommand);
             set.Bind(_buttonGoBack).For("Clicked").To(vm => vm.SendBackCommand);
             set.Apply();
+        }
+
+        private bool TextFieldResignFirstResponder(UITextField textField)
+        {
+            textField.ResignFirstResponder();
+            return true;
         }
     }
 }
